f87ed263a9dc57e3562d14f230f2414c0aeedb28
overkey <overkey010@gmail.com>
2025-10-08 00:19:08 +0300

    Day 5: exercises with lists

---
+++ day5_lists.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5] 
+total = 0
+
+for num in numbers:
+    total += num  
+print("Сумма =", total)   
+
+fruits = ["яблоко", "банан", "груша"]
+fruits.append("апельсин")   
+
+for fruit in fruits:  
+    print(fruit)     
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for n in nums:
+    if n % 2 == 0:
+        print(n)
+ыы
