07627eca532e42a8ec86c1370477bbe0d37c6b95
overkey <overkey010@gmail.com>
2025-10-10 01:05:49 +0300

    Day 8: string exercises

---
+++ day8_strings.py
@@ -0,0 +1,12 @@
+name = input("Ведите имя: ")
+print(name.upper())
+
+text = input("Введите строку: ")
+print("Длина строки =", len(text))
+
+sentence = input("Введите предложение: ")
+words = sentence.split()
+print(words)
+
+word = input("Ведите слово: ")
+print(word[::-1])
