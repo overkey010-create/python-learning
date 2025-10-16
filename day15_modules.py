550a06c937b3e11ca1b8e30381bbcb53fdf458ea
overkey <overkey010@gmail.com>
2025-10-16 22:53:55 +0300

    Day 15: modules and imports exercises

---
+++ day15_modules.py
@@ -0,0 +1,12 @@
+import math
+from random import choice
+from datetime import date
+
+
+print(math.sqrt(144))
+
+pets = ["кот", "собака", "птица"]
+print(choice(pets))
+
+now = date.today()
+print(now)
---
+++ main.py
@@ -0,0 +1,3 @@
+import helper
+
+helper.greet("Андрей")
