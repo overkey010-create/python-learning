b6c571894fee5b6e7fb01eb6d595a7f706123dac
overkey <overkey010@gmail.com>
2025-10-16 20:57:31 +0300

    Day 12: functions with lists and dicts

--- day12_functions_structures.py
+++ day12_functions_structures.py
@@ -1,18 +1,30 @@
-def max_in_list(lst): # Максимум в списке
+def max_in_list(lst): 
     maximum = max(lst)
     return maximum
 
 print(max_in_list([1, 7, 3, 9, 2])) 
 
 
-grades = {
+grades = {      
     "Иван": [5, 4, 3],
     "Мария": [5, 5, 4],
     "Андрей": [3, 4, 4]
 }
 
 def find_student(grades, name):
-    return grades.get("Мария")   # получаем оценки по имени
+    return grades.get("name")  
 
-print(find_student(grades, "Мария"))  # ожидаем [5, 5, 4]
+print(find_student(grades, "Мария"))
+
+
+def count_letters(word):
+    counts = {}
+    for letter in word:
+        if letter in counts:
+            counts[letter] += 1
+        else:
+            counts[letter] = 1
+    return counts
+
+print(count_letters("banana"))  
 
